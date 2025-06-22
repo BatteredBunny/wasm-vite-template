4b404db92ae4462d8a616404213677583306a122
BatteredBunny <ayes2022@protonmail.com>
2025-06-23 00:06:47 +0300

    feat: Bump deps, remove flake-utils & wee_alloc

--- flake.nix
+++ flake.nix
@@ -2,39 +2,64 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
-    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
-    { nixpkgs
+    { self
+    , nixpkgs
     , rust-overlay
-    , flake-utils
     , ...
     }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        overlays = [ (import rust-overlay) ];
-        pkgs = import nixpkgs {
-          inherit system overlays;
-        };
-        wasm-rust = pkgs.rust-bin.stable.latest.default.override {
-          extensions = [ "rust-src" ];
-          targets = [ "wasm32-unknown-unknown" ];
-        };
-      in
-      with pkgs; {
-        devShells.default = mkShell {
-          buildInputs = [
-            openssl
-            pkg-config
-            gnumake
-            wasm-rust
-            yarn
-            wasm-bindgen-cli
-          ];
-        };
-        packages.default = pkgs.callPackage ./build.nix { };
-      }
-    );
+    let
+      inherit (nixpkgs) lib;
+
+      systems = lib.systems.flakeExposed;
+
+      forAllSystems = lib.genAttrs systems;
+
+      nixpkgsFor = forAllSystems (system: import nixpkgs {
+        inherit system;
+
+        overlays = [
+          rust-overlay.overlays.default
+        ];
+      });
+    in
+    {
+      overlays.default = final: prev: {
+        {{project-name}} = self.packages.${final.stdenv.system}.{{project-name}};
+      };
+
+      packages = forAllSystems (system:
+        let
+          pkgs = nixpkgsFor.${system};
+        in
+        rec {
+          {{project-name}} = default;
+          default = pkgs.callPackage ./build.nix { };
+        }
+      );
+
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgsFor.${system};
+
+          wasm-rust = pkgs.rust-bin.stable.latest.default.override {
+            extensions = [ "rust-src" ];
+            targets = [ "wasm32-unknown-unknown" ];
+          };
+        in
+        {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              openssl
+              pkg-config
+              gnumake
+              wasm-rust
+              yarn
+              wasm-bindgen-cli
+            ];
+          };
+        });
+    };
 }
--- src/lib.rs
+++ src/lib.rs
@@ -3,12 +3,6 @@ mod utils;
 use gloo::{console::log, dialogs::alert, events::EventListener};
 use wasm_bindgen::prelude::*;
 
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
 #[wasm_bindgen]
 pub fn run() {
     utils::set_panic_hook(); // Error handling
